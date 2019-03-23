b6eb7eebecf301f2864da2cbb92c5c3c0ba11475
Jorge <jorgepalacioscl@gmail.com>
2019-03-22 22:06:51 -0300

    Todo

---
+++ src/components/shared/Loader.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .container
+    .content.has-text-centered.Loader
+      h1 Cargando ...
+</template>
+
+<style lang="scss">
+.Loader {
+  padding: 20px;
+  min-height: 40vh;
+}
+</style>
